7f7cfecc3654938b136f2daf1f9b8d511f8a77c5
santosh <pvsantosh@me.com>
2021-06-28 18:30:22 -0400

    Pushing empty repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import {
+  Platform,
+} from "@ionic/angular";
+import { CodePush, InstallMode } from "@ionic-native/code-push/ngx";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor(public platform: Platform,
+    private codePush: CodePush,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.codePush.notifyApplicationReady();
+      // Using this function to create custom message UI to the user.
+      this.codePush.checkForUpdate().then((update) => {
+        console.log("---> checking for update ", JSON.stringify(update));
+        if (!update) {
+          console.log("---> The app is up to date.");
+        } else {
+          console.log("---> An update is available! Should we download it?");
+          this.codePush
+            .sync(
+              {
+                installMode: InstallMode.ON_NEXT_RESTART,
+                updateDialog: {
+                  appendReleaseDescription: true,
+                  descriptionPrefix: "\n\n Change log:\n",
+                },
+              },
+              (progress) => {
+                console.log(
+                  `Downloaded ${progress.receivedBytes} of ${progress.totalBytes}`
+                );
+              }
+            )
+            .subscribe(
+              (data) => {
+                console.log("CODE PUSH SUCCESSFUL: " + data);
+                // this.codePush.restartApplication();
+              },
+              (err) => {
+                console.log("CODE PUSH ERROR: " + err);
+              }
+            );
+        }
+      });
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CodePush } from "@ionic-native/code-push/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, CodePush,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
